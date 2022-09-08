0d18f038e7a9191efb7b8243176b933c15cc25a8
Seth David Gyimah <esec26@gmail.com>
2022-09-08 18:16:06 +0000

    associations, many to many

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ActiveRecord::Base
   has_many :reviews
+  has_many :users, through: :reviews
 end
+
+# this means we can access a game and any user related to the game by posting a review about that game
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :game
+  belongs_to :user
 end
+
+# this means a new Review Instance can be created and associated
+# with a Game and a User.
